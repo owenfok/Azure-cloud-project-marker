309fa1b596ef09d3266f7442524f1431a9245938
owenfok <71066395+owenfok@users.noreply.github.com>
2021-04-22 20:49:53 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
+#Deploy the Stack
 pulumi up -y
+#output stack 
 pulumi stack export --file stack.json
+#output stack to bucket 
+#pulumi stack output -j $primaryStorageKey
+# mocha run test
 mocha -r ts-node/register tests/*.ts
